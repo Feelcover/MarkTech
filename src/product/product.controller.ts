c59cda0d7f0acc6db695296141b83cd35e9cce63
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-12-27 19:25:47 +0300

    править контроллер для products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, HttpCode, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { CurrentUser } from 'src/auth/decorators/user.decorator';
 import { ProductDto } from './dto/product.dto';
@@ -12,24 +22,26 @@ export class ProductController {
   @HttpCode(200)
   @Get()
   async getAll() {
-	  return this.productService.getAll()
+    return this.productService.getAll();
   }
 
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post()
+  async createProduct() {
+    return this.productService.create();
+  }
 
   @UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Auth()
-	@Post()
-	async createProduct() {
-		return this.productService.create()
-	}
-
-  @UsePipes(new ValidationPipe())
-	@HttpCode(200)
-	@Put(':id')
-	@Auth()
-	async updateProduct(@Param('id') id: string, @Body() dto: ProductDto, @CurrentUser('id') userId: number) {
-		return this.productService.update(+id, dto, userId)
-	}
-
+  @HttpCode(200)
+  @Put(':id')
+  @Auth()
+  async updateProduct(
+    @Param('id') id: string,
+    @Body() dto: ProductDto,
+    @CurrentUser('id') userId: number
+  ) {
+    return this.productService.update(+id, dto, userId);
+  }
 }
